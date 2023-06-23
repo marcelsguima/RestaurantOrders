43dc0bd869d8c89552351b96cd3c5599160b1663
marcelsguima <marcelsguima@gmail.com>
2023-06-23 15:31:12 -0300

    Req 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,30 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    queijo = Ingredient('queijo mussarela')
+    carne = Ingredient('carne')
+    ovo = Ingredient('ovo')
+    ovo2 = Ingredient('ovo')
+
+    # Test 1.1
+    assert hash(ovo) == hash(ovo2)
+
+    # Test 1.2
+    assert hash(ovo) != hash(queijo)
+
+    # Test 1.3
+    assert ovo == ovo2
+
+    # Test 1.4
+    assert queijo != carne
+
+    # Test 1.5
+    assert repr(carne) == "Ingredient('carne')"
+
+    # Test 1.6
+    assert ovo.name == 'ovo'
+
+    # Test 1.7
+    assert ovo.restrictions == {Restriction.ANIMAL_DERIVED}
